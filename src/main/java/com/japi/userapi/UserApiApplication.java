ec697d1cb778a50126ce1150a218cd64b8275844
Andre Camilo <andrehcamilo@gmail.com>
2025-06-06 00:10:33 -0300

    add healhcheck

--- src/main/java/com/japi/userapi/UserApiApplication.java
+++ src/main/java/com/japi/userapi/UserApiApplication.java
@@ -1,11 +1,28 @@
 package com.japi.userapi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class UserApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApiApplication.class, args);
     }
-} 
+
+    @Bean
+    public CommandLineRunner mongoHealthCheck(MongoTemplate mongoTemplate) {
+        return args -> {
+            try {
+                mongoTemplate.executeCommand("{ ping: 1 }");
+                System.out.println("[HEALTHCHECK] MongoDB is healthy");
+            } catch (Exception e) {
+                System.err.println("[HEALTHCHECK] MongoDB is unavailable: " + e.getMessage());
+                // Opcional: lançar exceção para impedir o start da aplicação
+                // throw new IllegalStateException("MongoDB is unavailable", e);
+            }
+        };
+    }
+}
